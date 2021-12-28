2a60ea33767ab3be0040d488a4ecbe4d32a3b65c
huachao0326 <huachao0326@126.com>
2021-12-28 17:19:14 +0800

    rollback

--- src/data/TotalSupply.ts
+++ src/data/TotalSupply.ts
@@ -4,7 +4,7 @@ import { useTokenContract } from '../hooks/useContract'
 import { useSingleCallResult } from '../state/multicall/hooks'
 
 // returns undefined if input token is undefined, or fails to get token contract,
-// or contract total supply cannot be  fetched
+// or contract total supply cannot be fetched
 export function useTotalSupply(token?: Token): TokenAmount | undefined {
   const contract = useTokenContract(token?.address, false)
 
